a7272b6b882d2530b17479de0a973cb8732f5aef
Eduardo <eduardooffside@gmail.com>
2021-07-12 23:08:34 -0300

    style profile relations

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import GridMain from '../src/components/GridMain'
 import Box from '../src/components/Box'
 import { AlurakutMenu } from '../src/libs/AluraCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/css/ProfileRelations'
 
 function ProfileSideBar(props) {
   return (
@@ -13,6 +14,15 @@ function ProfileSideBar(props) {
 export default function Home() {
   const githubUser = 'dududueedu'
 
+  const amigosFavoritos = [
+    'kelyAna',
+    'Vafernandes',
+    'laysqueiroz',
+    'VitinhoSouza',
+    'HugoPDF5',
+    'daianefsilveira'
+  ]
+
   return (
     <>
       <AlurakutMenu />
@@ -21,11 +31,27 @@ export default function Home() {
           <ProfileSideBar User={githubUser} />
         </div>
         <div>
-          <Box style={{ gridArea: 'welcome' }}>Bem vindo</Box>
+          <Box style={{ gridArea: 'welcome' }}>
+            <h1 className="title"> Bem vindo(a) </h1>
+          </Box>
         </div>
         <div style={{ gridArea: 'profileRelations' }}>
-          <Box>Pessoas da comunidade</Box>
-          <Box>Comunidades</Box>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle"> Pessoas da comunidade ({amigosFavoritos.length}) </h2>
+
+            <ul>
+              {amigosFavoritos.map((amigo) => {
+                return (
+                  <li>
+                    <a href={`https://github.com/${amigo}`} key={amigo} target="_blank">
+                      <img src={`https://github.com/${amigo}.png`} />
+                      <span> {amigo} </span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
         </div>
       </GridMain>
     </>
